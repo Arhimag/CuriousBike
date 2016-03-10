65183c691ee5ce5d9c1661022b544b5c9fd37767
Maxim.Statsenko <MaxStatsenko@gmail.com>
2016-03-10 17:49:08 +0300

    Working BackgroundLocationService2

--- app/src/main/java/net/arhimag/curiousbike/DownloadService.java
+++ app/src/main/java/net/arhimag/curiousbike/DownloadService.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
@@ -23,42 +24,59 @@ public class DownloadService extends IntentService {
     }
     @Override
     protected void onHandleIntent(Intent intent) {
+        Log.d("BikeDownload","1");
         String urlToDownload = intent.getStringExtra("url");
         String pathToOutput = intent.getStringExtra("destination");
         ResultReceiver receiver = (ResultReceiver) intent.getParcelableExtra("receiver");
-
+        Log.d("BikeDownload","2");
         try {
             URL url = new URL(urlToDownload);
+            Log.d("BikeDownload","3");
             URLConnection connection = url.openConnection();
+            Log.d("BikeDownload","4");
             connection.connect();
+            Log.d("BikeDownload", "5");
             // this will be useful so that you can show a typical 0-100% progress bar
             int fileLength = connection.getContentLength();
-
+            Log.d("BikeDownload","6");
             // download the file
             InputStream input = new BufferedInputStream(connection.getInputStream());
+            Log.d("BikeDownload","7");
             OutputStream output = new FileOutputStream(pathToOutput);
+            Log.d("BikeDownload","8");
 
             byte data[] = new byte[1024];
             long total = 0;
             int count;
             while ((count = input.read(data)) != -1) {
+                Log.d("BikeDownload","9");
                 total += count;
                 // publishing the progress....
                 Bundle resultData = new Bundle();
-                resultData.putInt("progress" ,(int) (total * 100 / fileLength));
+                Log.d("BikeDownload","10");
+                resultData.putInt("progress", (int) (total * 100 / fileLength));
+                Log.d("BikeDownload", "11");
                 receiver.send(UPDATE_PROGRESS, resultData);
+                Log.d("BikeDownload", "12");
                 output.write(data, 0, count);
+                Log.d("BikeDownload", "13");
             }
-
+            Log.d("BikeDownload","14");
             output.flush();
+            Log.d("BikeDownload", "15");
             output.close();
+            Log.d("BikeDownload", "16");
             input.close();
+            Log.d("BikeDownload", "17");
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        Log.d("BikeDownload","18");
         Bundle resultData = new Bundle();
-        resultData.putInt("progress" ,100);
+        Log.d("BikeDownload","19");
+        resultData.putInt("progress", 100);
+        Log.d("BikeDownload", "20");
         receiver.send(UPDATE_PROGRESS, resultData);
+        Log.d("BikeDownload", "21");
     }
 }
